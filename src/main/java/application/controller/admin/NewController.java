97d99ad15e65326edd64fc11e265a72c1639f168
=trieu.ngoquang <=ngoquangtrieu99tb@gmail.com>
2020-02-16 10:12:26 +0700

    route

--- src/main/java/application/controller/admin/NewController.java
+++ src/main/java/application/controller/admin/NewController.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = { "/admin-new-list" })
+@WebServlet(urlPatterns = { "/admin-new" })
 public class NewController extends HttpServlet {
 	/**
 	 * 
